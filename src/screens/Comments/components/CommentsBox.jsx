a359a0b643dd7567faa3a108e5b9f03e4ded8fce
JorshGL <jorge.gl0905@gmail.com>
2023-04-19 21:52:19 -0500

    comments

--- src/screens/Comments/components/CommentsBox.jsx
+++ src/screens/Comments/components/CommentsBox.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 
 const CommentsBox = ({ comments }) => {
+  const navigate = useNavigate()
   return (
     <div className="flex flex-col gap-3 ml-10 mr-5">
       {comments.length > 0 &&
         comments.map((comment, index) => (
-          <div key={index} className="flex gap-3">
+          <div key={index} className="flex gap-3" onClick={() => navigate(`/profile/${comment.user.id}`)}>
             <div className="flex items-center self-start aspect-square justify-center rounded-full overflow-hidden p-[1px] bg-gradient-to-tr from-custom-yellow-main via-custom-red-main to-custom-purple-main">
               <img
                 src={comment.user.picture.thumbnail}
